e88ec464a46149ea40b8cd57cb375bbf2081512d
Ilyasse Essadak Samaali <developer.elias.essadak@gmail.com>
2022-06-05 15:27:30 +0200

    Agregando la parte backend.

---
+++ Backend/employeecontrol/src/main/java/com/eliaskrr/employeecontrol/EmployeecontrolApplication.java
@@ -0,0 +1,13 @@
+package com.eliaskrr.employeecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeecontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeecontrolApplication.class, args);
+	}
+
+}
---
+++ Backend/employeecontrol/src/test/java/com/eliaskrr/employeecontrol/EmployeecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliaskrr.employeecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
